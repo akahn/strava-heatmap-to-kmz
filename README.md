e33e38c227ac631d37359d8cd3d7de37799b15e3
Alex Kahn <alex@librato.com>
2016-01-16 17:02:06 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# strava-heatmap-to-kmz
+
+Generate KMZ files from the [Strava Global Heatmap](http://labs.strava.com/heatmap/). This allows you to use the heatmap as a map layer (currently 1000×1000 pixels) on a mobile device (e.g. iPhone or Garmin), which enabling offline use and helps you to locate where the hell you are in the woods.
+
+## Dependencies
+
+* Node.js
+* NPM
+* Phantom.js 2.0
+
+## Usage
+
+`npm run generate <lat,lon> <output_file>`
+
+For example:
+
+`npm run generate 42.32733,-72.70095 /Users/akahn/strava.kmz`
+
+If you encounter issues, try running in debug mode:
+
+`npm run debug 42.32733,-72.70095 /Users/akahn/strava.kmz`
+
+This makes Phantom.js write its debug log to stdout.
+
+## How it works
+
+The program loads the Strava Heatmap in a headless WebKit web browser via PhantomJS and takes a snapshot of the page and saves the map bounds. It then constructs a KML file declaring an image overlay positioned using the map bounds loaded previously. It then zips up this KML file along with the snapshot image, creating a KMZ that can be used in Google Earth and other programs.
+
+## Problems
+
+It doesn't work. The georeferencing is broken. The map overlay, when loaded in Google Earth, doesn't line up with the base map. This could be because of differing map projections used by Google Earth vs. Strava, or an error in calculating the map bounds.
---
+++ main.js
@@ -0,0 +1,60 @@
+var page = require('webpage').create(),
+    system = require('system'),
+    fs = require('fs'),
+    zip = new require('node-zip')(),
+    _ = require('lodash-node');
+
+if (system.args.length != 3) {
+  console.warn("Must provide lat,lon and output directory arguments");
+  phantom.exit();
+}
+
+var outputFile = system.args[system.args.length - 1],
+    center = system.args[system.args.length - 2];
+
+try {
+  fs.write(outputFile, '');
+}
+catch (e) {
+  console.warn('Cannot write to file ' + outputFile);
+  phantom.exit();
+}
+
+if (center.split(',').length != 2) {
+  console.warn('Must supply lat,lon in the form of lat,lon')
+  phantom.exit();
+}
+else {
+  var latlon = center.split(','),
+      lat = latlon[0],
+      lon = latlon[1];
+}
+
+var zoom = 15,
+    size = 1000,
+    east, west, north, south, bounds;
+
+var kmlTemplate = _.template(fs.read('kml.template', 'utf8'));
+
+page.viewportSize = { width: size, height: size };
+page.open('http://labs.strava.com/heatmap/#15/' + lon + '/' + lat + '/gray/bike', function() {
+  var bounds = page.evaluate(function() {
+    $('#header, #controls, #sidebar').remove();
+    $(window).trigger('resize');
+    var bounds = map.getBounds();
+    return {east: bounds.getEast(),
+            west: bounds.getWest(),
+            north: bounds.getNorth(),
+            south: bounds.getSouth()};
+  });
+
+  page.render('strava.png', {quality: 100});
+  var kml = kmlTemplate(_.merge(bounds, {lat: lat, lon: lon}));
+  zip.file('strava.png', fs.read('strava.png', {mode: 'rb'}), {binary: true});
+  zip.file('doc.kml', kml);
+  fs.write(outputFile, zip.generate({type: 'string', compression: 'DEFLATE'}), 'wb');
+  fs.remove('strava.png');
+
+  phantom.exit()
+});
+
